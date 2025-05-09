370e24ee3d156cb630fa59c2f7a1c9d7ed71a922
FAHIM MUNTASHIR RATUL <H1566N@hallgato.nje.hu>
2025-05-09 22:09:41 +0200

    Initial project structure and config.php (by Ratul)

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'menus' => [
+        'mainpage' => 'Mainpage',
+        'images' => 'Images',
+        'contact' => 'Contact',
+        'messages' => 'Messages',
+        'login' => 'Login',
+        'logout' => 'Logout'
+    ],
+    'db' => [
+        'dsn' => 'mysql:host=localhost;dbname=datab1',
+        'user' => 'your_db_user',
+        'pass' => 'your_db_pass'
+    ]
+];
+?>
