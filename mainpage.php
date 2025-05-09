c51c9467ec26004fd815f65280dc21a7b807aef6
aminul23453 <mdaminul990011@gmail.com>
2025-05-09 23:30:04 +0200

    Created mainpage with videos and map (by Aminul)

---
+++ mainpage.php
@@ -0,0 +1,14 @@
+<?php
+<div>
+<h2>Welcome to Currency Exchange!</h2>
+<p>Exchange rates, tips, and more.</p>
+<video width="320" height="240" controls>
+<source src="uploads/shortvideo.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+</video>
+<iframe width="320" height="240" src="https://www.youtube.com/embed/8X9Vjv3bP6k" allowfullscreen></iframe>
+<div>
+<h3>Our Location</h3>
+<iframe src="https://www.google.com/maps?q=Budapest&t=&z=13&ie=UTF8&iwloc=&output=embed" width="400" height="300"></iframe>
+</div>
+</div>
