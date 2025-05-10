64f4cca714fc7e317a19d0b9dda0bc0c69b745c9
FAHIM MUNTASHIR RATUL <H1566N@hallgato.nje.hu>
2025-05-10 08:25:56 +0200

    Updated mainpage.php (by Aminul)

--- controllers/mainpage.php
+++ controllers/mainpage.php
@@ -1,12 +1,10 @@
-
 <div>
     <h2>Welcome to Currency Exchange!</h2>
     <p>Exchange rates, tips, and more.</p>
     <video width="320" height="240" controls>
-        <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
+        <source src="uploads/vecteezy_electronic-scoreboard-with-the-exchange-rate-on-the-street_17677319.mp4" type="video/mp4">
         Your browser does not support the video tag.
     </video>
-    <iframe width="320" height="240" src="https://www.youtube.com/embed/8X9Vjv3bP6k" allowfullscreen></iframe>
     <div>
         <h3>Our Location</h3>
         <iframe 
