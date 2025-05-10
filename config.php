b7477f42995b0cff871e8357807e253b6ddf3d42
FAHIM MUNTASHIR RATUL <H1566N@hallgato.nje.hu>
2025-05-10 06:42:28 +0200

    Updated config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // Database connection using PDO with your provided credentials
 $dbh = new PDO(
     'mysql:host=localhost;dbname=datab4',
-    'pqfbdz',
+    'datab4',
     'Munn@2002',
     array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
 );
