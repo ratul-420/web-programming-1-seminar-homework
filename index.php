1ae4a1fa53e4e06d800491e70210fea9f3b9c3bc
FAHIM MUNTASHIR RATUL <H1566N@hallgato.nje.hu>
2025-05-10 06:58:54 +0200

    Updated index.php (by Ratul)

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-<?php 
+<?php
+// Show all errors for debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 $config = require 'config.php';
 
@@ -17,8 +22,8 @@ require 'templates/header.php';
 require 'templates/menu.php';
 
 $controller = "controllers/$menu.php";
-if (file_exists(filename: $controllers)) {
-    require $controllers;
+if (file_exists($controller)) {
+    require $controller;
 } else {
     echo "<h2>Page not found</h2>";
 }
